db302aebfdebba93cfefef51ca4e9ca1ea6fa8e7
Trinh Vinh Thuan <thuantrinh7845@gmail.com>
2024-09-23 19:30:04 +0700

    updating exercise 10

--- Core/Src/ex10.c
+++ Core/Src/ex10.c
@@ -8,8 +8,10 @@
 #include "ex10.h"
 
 void ex10_run(int sec, int min, int hour){
+	min = (min / 5) % 12;
+	sec = (sec / 5) % 12;
 	while (1){
-		  if (sec >= 12){
+		  if (sec >= 60){
 			  sec = 0;
 			  clearNumberOnClock(min);
 			  min++;
@@ -22,12 +24,12 @@ void ex10_run(int sec, int min, int hour){
 		  if (hour >= 12){
 			  hour = 0;
 		  }
-		  setNumberOnClock(sec);
+		  setNumberOnClock(sec % 12);
 		  setNumberOnClock(min);
 		  setNumberOnClock(hour);
 
-		  HAL_Delay(1000);
-		  clearNumberOnClock(sec);
+		  HAL_Delay(100);
+		  clearNumberOnClock(sec % 12);
 		  sec++;
 	}
 }
